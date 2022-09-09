7e5ed42438cfe3e0bb4103258ef3e64a8c30c19a
CheYH <08822407d@gmail.com>
2022-09-09 08:01:17 -0700

    creat

--- SXfieldscheck/Form1.Designer.cs
+++ SXfieldscheck/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SXfieldscheck
 {
-	partial class Form1
+	partial class FrmMain
 	{
 		/// <summary>
 		/// 必需的设计器变量。
@@ -28,13 +28,96 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.tbx_inputDIR = new System.Windows.Forms.TextBox();
+			this.btn_inputDIR = new System.Windows.Forms.Button();
+			this.tbx_outputDIR = new System.Windows.Forms.TextBox();
+			this.btn_outputDIR = new System.Windows.Forms.Button();
+			this.btn_start = new System.Windows.Forms.Button();
+			this.btn_cancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// tbx_inputDIR
+			// 
+			this.tbx_inputDIR.AllowDrop = true;
+			this.tbx_inputDIR.Location = new System.Drawing.Point(165, 22);
+			this.tbx_inputDIR.Name = "tbx_inputDIR";
+			this.tbx_inputDIR.Size = new System.Drawing.Size(434, 25);
+			this.tbx_inputDIR.TabIndex = 3;
+			// 
+			// btn_inputDIR
+			// 
+			this.btn_inputDIR.Location = new System.Drawing.Point(21, 22);
+			this.btn_inputDIR.Name = "btn_inputDIR";
+			this.btn_inputDIR.Size = new System.Drawing.Size(126, 25);
+			this.btn_inputDIR.TabIndex = 2;
+			this.btn_inputDIR.Text = "选择输入文件夹";
+			this.btn_inputDIR.UseVisualStyleBackColor = true;
+			this.btn_inputDIR.Click += new System.EventHandler(this.btn_inputDIR_Click);
+			// 
+			// tbx_outputDIR
+			// 
+			this.tbx_outputDIR.AllowDrop = true;
+			this.tbx_outputDIR.Location = new System.Drawing.Point(165, 75);
+			this.tbx_outputDIR.Name = "tbx_outputDIR";
+			this.tbx_outputDIR.Size = new System.Drawing.Size(434, 25);
+			this.tbx_outputDIR.TabIndex = 5;
+			// 
+			// btn_outputDIR
+			// 
+			this.btn_outputDIR.Location = new System.Drawing.Point(21, 75);
+			this.btn_outputDIR.Name = "btn_outputDIR";
+			this.btn_outputDIR.Size = new System.Drawing.Size(126, 25);
+			this.btn_outputDIR.TabIndex = 4;
+			this.btn_outputDIR.Text = "选择输出文件夹";
+			this.btn_outputDIR.UseVisualStyleBackColor = true;
+			this.btn_outputDIR.Click += new System.EventHandler(this.btn_outputDIR_Click);
+			// 
+			// btn_start
+			// 
+			this.btn_start.Location = new System.Drawing.Point(165, 130);
+			this.btn_start.Name = "btn_start";
+			this.btn_start.Size = new System.Drawing.Size(108, 27);
+			this.btn_start.TabIndex = 6;
+			this.btn_start.Text = "开始处理";
+			this.btn_start.UseVisualStyleBackColor = true;
+			this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+			// 
+			// btn_cancel
+			// 
+			this.btn_cancel.Location = new System.Drawing.Point(345, 130);
+			this.btn_cancel.Name = "btn_cancel";
+			this.btn_cancel.Size = new System.Drawing.Size(108, 27);
+			this.btn_cancel.TabIndex = 7;
+			this.btn_cancel.Text = "取消";
+			this.btn_cancel.UseVisualStyleBackColor = true;
+			this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+			// 
+			// FrmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(629, 183);
+			this.Controls.Add(this.btn_cancel);
+			this.Controls.Add(this.btn_start);
+			this.Controls.Add(this.tbx_outputDIR);
+			this.Controls.Add(this.btn_outputDIR);
+			this.Controls.Add(this.tbx_inputDIR);
+			this.Controls.Add(this.btn_inputDIR);
+			this.Name = "FrmMain";
+			this.Text = "字段检查";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox tbx_inputDIR;
+		private System.Windows.Forms.Button btn_inputDIR;
+		private System.Windows.Forms.TextBox tbx_outputDIR;
+		private System.Windows.Forms.Button btn_outputDIR;
+		private System.Windows.Forms.Button btn_start;
+		private System.Windows.Forms.Button btn_cancel;
 	}
 }
 
--- SXfieldscheck/Form1.cs
+++ SXfieldscheck/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,274 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Utils;
+
 namespace SXfieldscheck
 {
-	public partial class Form1 : Form
+	public partial class FrmMain : Form
 	{
-		public Form1()
+		public FrmMain()
 		{
 			InitializeComponent();
+		}
+
+		private void btn_inputDIR_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd =new FolderBrowserDialog();
+			fbd.Description = "请选择文件夹";
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				string dir = fbd.SelectedPath;
+				if (dir != null)
+				{
+					tbx_inputDIR.Text = dir;
+				}
+			}
+		}
+
+		private void btn_outputDIR_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd =new FolderBrowserDialog();
+			fbd.Description = "请选择文件夹";
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				string dir = fbd.SelectedPath;
+				if (dir != null)
+				{
+					tbx_outputDIR.Text = dir;
+				}
+			}
+		}
+
+		private void btn_start_Click(object sender, EventArgs e)
+		{
+			string input_dir = tbx_inputDIR.Text;
+			string output_dir = tbx_outputDIR.Text;
+
+			input_dir = @"C:\Users\cheyh\Desktop\testdata\xSXcheck";
+			output_dir = @"C:\Users\cheyh\Desktop\testdata\xSXresult";
+
+			if (!Directory.Exists(input_dir))
+			{
+				MessageBox.Show("错误：输入文件夹不存在");
+				return;
+			}
+			if (!Directory.Exists(output_dir))
+			{
+				MessageBox.Show("错误：输出文件夹不存在");
+				return;
+			}
+
+			List<string> jsx_files = Utils.FIO.traverseSearchFile_Ext(input_dir, ".jsx");
+			List<string> bsx_files = Utils.FIO.traverseSearchFile_Ext(input_dir, ".bsx");
+			List<string> dsx_files = Utils.FIO.traverseSearchFile_Ext(input_dir, ".dsx");
+			List<string> rsx_files = Utils.FIO.traverseSearchFile_Ext(input_dir, ".rsx");
+			List<string> isx_files = Utils.FIO.traverseSearchFile_Ext(input_dir, ".isx");
+
+			foreach (string f in jsx_files)
+				check_jsx(f, input_dir, output_dir);
+			foreach (string f in bsx_files)
+				check_bsx(f, input_dir, output_dir);
+			foreach (string f in dsx_files)
+				check_dsx(f, input_dir, output_dir);
+			foreach (string f in rsx_files)
+				check_rsx(f, input_dir, output_dir);
+			foreach (string f in isx_files)
+				check_isx(f, input_dir, output_dir);
+		}
+
+		private void btn_cancel_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		void check_jsx(string f, string in_dir, string out_dir)
+		{
+			string outfile = f.Replace(in_dir, out_dir);
+			string dir = Path.GetDirectoryName(outfile);
+			if (!Directory.Exists(dir))
+				Directory.CreateDirectory(dir);
+			File.Create(outfile).Close();
+
+			StreamWriter sw = new StreamWriter(outfile);
+			string[] lines = File.ReadAllLines(f);
+			foreach (string line in lines)
+			{
+				char[] delimiterChars = { ' ', '\t' };
+				string[] fields = line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+				if (fields.Length == 12)
+				{
+					string number = fields[0];
+					string code = fields[1];
+					string err = number;
+					if (code.Equals("200206"))
+					{
+						double digit = 0;
+						if(Double.TryParse(fields[6], out digit))
+						{
+							string[] valstr = fields[6].Split('.');
+							Int64 integer;
+							Int64.TryParse(valstr[0], out integer);
+							if ((integer >= 3 && !valstr[1].Equals("00")) ||
+								(integer < 3 && valstr[1][1] != '0'))
+									err += "\t字段格式违规";
+						}
+						else
+						{
+							err += "\t字段非数值";
+						}
+					}
+
+					if(err.Length > number.Length)
+						sw.WriteLine(err);
+				}
+			}
+			sw.Flush();
+			sw.Close();
+		}
+
+		void check_bsx(string f, string in_dir, string out_dir)
+		{
+			string outfile = f.Replace(in_dir, out_dir);
+			string dir = Path.GetDirectoryName(outfile);
+			if (!Directory.Exists(dir))
+				Directory.CreateDirectory(dir);
+			File.Create(outfile).Close();
+
+			StreamWriter sw = new StreamWriter(outfile);
+			string[] lines = File.ReadAllLines(f);
+			foreach (string line in lines)
+			{
+				char[] delimiterChars = { ' ', '\t' };
+				string[] fields = line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+				if (fields.Length == 10)
+				{
+					string number = fields[0];
+					string code = fields[1];
+					string err = number;
+					if (code.Equals("120101") ||
+						code.Equals("120102") ||
+						code.Equals("120103") ||
+						code.Equals("120106") ||
+						code.Equals("120201") ||
+						code.Equals("120301"))
+					{
+						double digit = 0;
+						if(Double.TryParse(fields[5], out digit))
+						{
+							string[] valstr = fields[5].Split('.');
+							if (!valstr[1].Equals("00"))
+									err += "\t字段格式违规";
+						}
+						else
+						{
+							err += "\t字段非数值";
+						}
+					}
+
+					if(err.Length > number.Length)
+						sw.WriteLine(err);
+				}
+			}
+			sw.Flush();
+			sw.Close();
+		}
+
+		void check_dsx(string f, string in_dir, string out_dir)
+		{
+			string outfile = f.Replace(in_dir, out_dir);
+			string dir = Path.GetDirectoryName(outfile);
+			if (!Directory.Exists(dir))
+				Directory.CreateDirectory(dir);
+			File.Create(outfile).Close();
+
+			StreamWriter sw = new StreamWriter(outfile);
+			string[] lines = File.ReadAllLines(f);
+			foreach (string line in lines)
+			{
+				char[] delimiterChars = { ' ', '\t' };
+				string[] fields = line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+				if (fields.Length == 21)
+				{
+					string number = fields[0];
+					string code = fields[1];
+					string err = number;
+
+
+					if(err.Length > number.Length)
+						sw.WriteLine(err);
+				}
+			}
+			sw.Flush();
+			sw.Close();
+		}
+
+		void check_rsx(string f, string in_dir, string out_dir)
+		{
+			string outfile = f.Replace(in_dir, out_dir);
+			string dir = Path.GetDirectoryName(outfile);
+			if (!Directory.Exists(dir))
+				Directory.CreateDirectory(dir);
+			File.Create(outfile).Close();
+
+			StreamWriter sw = new StreamWriter(outfile);
+			string[] lines = File.ReadAllLines(f);
+			foreach (string line in lines)
+			{
+				char[] delimiterChars = { ' ', '\t' };
+				string[] fields = line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+				if (fields.Length == 8)
+				{
+					string number = fields[0];
+					string code = fields[1];
+					string err = number;
+					if (code.Equals("280309") ||
+						code.Equals("280401") ||
+						code.Equals("280402") ||
+						code.Equals("280500") ||
+						code.Equals("280501") ||
+						code.Equals("280600") ||
+						code.Equals("280800"))
+					{
+						err += "\t非法编码";
+					}
+
+					if(err.Length > number.Length)
+						sw.WriteLine(err);
+				}
+			}
+			sw.Flush();
+			sw.Close();
+		}
+
+		void check_isx(string f, string in_dir, string out_dir)
+		{
+			string outfile = f.Replace(in_dir, out_dir);
+			string dir = Path.GetDirectoryName(outfile);
+			if (!Directory.Exists(dir))
+				Directory.CreateDirectory(dir);
+			File.Create(outfile).Close();
+
+			StreamWriter sw = new StreamWriter(outfile);
+			string[] lines = File.ReadAllLines(f);
+			foreach (string line in lines)
+			{
+				char[] delimiterChars = { ' ', '\t' };
+				string[] fields = line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+				if (fields.Length == 14)
+				{
+					string number = fields[0];
+					string code = fields[1];
+					string err = number;
+					if (code.Equals("190102") && !fields[8].Equals("-32767.00"))
+						err += "\t字段值违规";
+
+					if(err.Length > number.Length)
+						sw.WriteLine(err);
+				}
+			}
+			sw.Flush();
+			sw.Close();
 		}
 	}
 }
